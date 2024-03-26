6c456fdc97526fabaae33ecc9131a9c633f1bf66
Moeez Iqbal <moeeziqbalmughal@gmail.com>
2024-03-26 23:01:08 +0500

    Connect to Database

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,10 @@
+import "dotenv/config.js";
 import Express from "express"
+import { connectDataBase } from "./db/config.js";
 
 const app = Express()
-
+connectDataBase();
+console.log(process.env, "DB_HOST");
 
 
 app.listen(3000, () =>console.log("Server Is Running on Port 3000"));
