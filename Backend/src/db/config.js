30aee3d0527d44a66328e882f900d96e6d52418f
Moeez Iqbal <moeeziqbalmughal@gmail.com>
2024-08-03 15:09:31 +0500

    change

--- Backend/src/db/config.js
+++ Backend/src/db/config.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize"
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+    host: 'aws-0-ap-southeast-1.pooler.supabase.com',
     port: process.env.DB_PORT,
     dialect: 'postgres'
 });
